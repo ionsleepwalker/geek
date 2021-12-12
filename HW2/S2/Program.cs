6ced6798f6896c01aae10b9577936a7daa01e84e
Dimitriy <night_blade_samurai@mail.ru>
2021-12-12 19:07:02 +0600

    add Solution 2

---
+++ HW2/S2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Веретенников Димитрий
+/*2. Написать метод подсчета количества цифр числа.*/
+Console.WriteLine("Программа подсчета количества цифр числа");
+string a = Console.ReadLine();
+int count = 0;
+foreach (char c in a){
+    if(Char.IsNumber(c)) {
+        count++; 
+    }
+}
+Console.WriteLine("Количество цифр в числе: " + count);
+
+
