bf78008deafbb9cc1b3f16953faa5bc97b79fcb5
Dimitriy <night_blade_samurai@mail.ru>
2021-12-12 17:19:52 +0600

    First solution

---
+++ HW2/S1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Веретенников Димитрий
+/*1. Рассчитать минимальное из трёх чисел.*/
+Console.Write("Программа подсчета минимального из трех чисел\nВведите первое значение: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое значение: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое значение: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a < b && a < c){
+    Console.WriteLine("Минимальное значение a = " + a);
+} else if(b < a && b < c) {
+    Console.WriteLine("Минимальное значение b = " + b);
+} else {
+    Console.WriteLine("Минимальное значение c = " + c);
+}
